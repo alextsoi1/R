797c5df5d98772332fee4b06abf4d0ba1a3b3cc4
alextsoi1 <tsoi.teen@gmail.com>
2024-11-27 18:55:46 -0500

    add parquet2csv.R

--- parquet2csv.R
+++ parquet2csv.R
@@ -1,1 +1,17 @@
+args <- commandArgs(trailingOnly = TRUE)
 
+if (length(args) < 2) {
+  ###### HZ edited ######
+  print("Usage: input.parquet.file    output.csv")
+  #######################
+  quit(status = 1)
+}
+
+
+library(data.table,lib.loc="~/R/R-4.4/lib/")
+input <- args[1]
+output <- args[2]
+
+a=as.data.table(arrow::read_parquet(input))
+
+write.table(a,file=output,row.names=F,col.names=T,sep=",",quote=F)
